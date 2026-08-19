72de052e857aff0f802cad5bb8e294732283b801
Tejas Padaki <tejaspadaki33@gmail.com>
2026-08-19 18:00:07 +0530

    chore: optimize build output by implementing manual chunking and increasing warning limit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1200,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('lucide-react')) return 'icons';
+            if (id.includes('react') || id.includes('@tanstack')) return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
